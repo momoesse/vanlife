15ce3aa9f52a3c7b1faed5a1fa146a028158fd64
Monica <112439372+momoesse@users.noreply.github.com>
2024-05-08 16:07:40 +0200

    fetch request to miragejs server

--- src/components/Vans.js
+++ src/components/Vans.js
@@ -1,7 +1,33 @@
-import React from "react"
+import React from "react";
 
 export default function Vans() {
+    const [allVans, setAllVans] = React.useState([]);
+
+    React.useEffect(() => {
+        fetch("/api/vans")
+            .then(res => res.json())
+            .then(data => setAllVans(data.vans))
+    }, [])
+
+    console.log(allVans);
+
+    const elementsToDisplay = allVans.map(el =>
+        <div key={el.id} className="van--container">
+            <img src={el.imageUrl} />
+            <div>
+                <h3>{el.name}</h3>
+                <p>€{el.price}<span>/day</span></p>
+                <i className="van--type">{el.type}</i>
+            </div>
+        </div>
+        )
+
     return (
-        <h1>This is my Vans components</h1>
+        <div>
+            <h2>Explore our van options</h2>
+            <div className="vans--container">
+            {elementsToDisplay}
+        </div>
+        </div>
     )
 }
