42c88dd4bd13d81d7b3e3d638e45d625b616e13c
Monica <112439372+momoesse@users.noreply.github.com>
2024-05-08 12:35:50 +0200

    mirage js set up

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home.js";
@@ -9,10 +8,12 @@ function App() {
   return (
     <BrowserRouter>
       <header>
-        <nav>
-          <Link to="/">#VANLIFE</Link>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
+        <nav className="nav--container">
+          <ul>
+            <li><Link to="/">#VANLIFE</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/vans">Vans</Link></li>
+          </ul>
         </nav>
       </header>
       <Routes>
