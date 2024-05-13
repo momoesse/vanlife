38cd41e6e5cb106630b705b808de7b5514fd43b7
Monica <112439372+momoesse@users.noreply.github.com>
2024-05-13 19:44:20 +0200

    using links to add search params

--- src/components/Vans/Vans.js
+++ src/components/Vans/Vans.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useSearchParams, Link } from "react-router-dom";
 
 export default function Vans() {
     const [allVans, setAllVans] = React.useState([]);
@@ -10,9 +10,20 @@ export default function Vans() {
             .then(data => setAllVans(data.vans))
     }, [])
 
-    console.log(allVans);
+    const [searchParams, setSearchParams] = useSearchParams();
 
-    const elementsToDisplay = allVans.map(el =>
+    const uniqueTypes = [...new Set(allVans.map((el) => el.type))];
+    const filterBar = uniqueTypes.map(el => 
+        <Link to={`?type=${el}`}><span className="badge">{el}</span></Link>
+    )
+
+    const typeFilter = searchParams.get("type");
+    console.log(typeFilter);
+
+    const displayedVans = typeFilter ? 
+        allVans.filter( el => el.type === typeFilter) : allVans;
+
+    const elementsToDisplay = displayedVans.map(el =>
         <Link to={`/vans/${el.id}`} aria-label={`View details for ${el.name}, priced at ${el.price} per day`}>
             <div key={el.id} className="van--container">
                 <img src={el.imageUrl} alt={`Image of ${el.name}`} />
@@ -23,12 +34,15 @@ export default function Vans() {
                 </div>
             </div>
         </Link>
-
     )
 
     return (
         <div>
             <h2>Explore our van options</h2>
+            <div>
+                {filterBar}
+                <Link to=".">Clear filters</Link>
+            </div>
             <div className="vans--container">
                 {elementsToDisplay}
             </div>
