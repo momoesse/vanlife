5a24cdaafb7fcdccaa8f660fb22f0286eed7aa60
Monica <112439372+momoesse@users.noreply.github.com>
2024-05-20 22:11:33 +0200

    fix redirect to login page

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,12 @@
 import { redirect } from "react-router-dom";
 
 export async function requireAuth() {
-    const isLoggedIn = true;
-    
+    const isLoggedIn = false;
+
     if (!isLoggedIn) {
-        throw redirect("/login?message=You must log in first")
-    } 
+        const response = redirect("/login?message=You must log in first")
+        response.body = true
+        throw response
+    }
+    return null
 }
