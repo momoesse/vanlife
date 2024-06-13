95a541f0df222869c7e8d2a97779d1ef6cf99e38
Monica <112439372+momoesse@users.noreply.github.com>
2024-06-13 16:01:44 +0200

    defer, Await, Suspense in Vans component

--- src/components/Vans/Vans.js
+++ src/components/Vans/Vans.js
@@ -1,14 +1,15 @@
-import React from "react";
-import { useSearchParams, useLoaderData, Link } from "react-router-dom";
+import React, { Suspense } from "react";
+import { useSearchParams, useLoaderData, Link, defer, Await } from "react-router-dom";
 import { getVans } from "../../api.js";
 
 export function loader() {
-    return getVans();
+    return defer({ vans: getVans() });
 }
 
 export default function Vans() {
     const [searchParams, setSearchParams] = useSearchParams();
-    const allVans = useLoaderData();
+    const promiseData = useLoaderData();
+    const typeFilter = searchParams.get("type");
 
     function handleFilterChange(key, value) {
         setSearchParams(prevParams => {
@@ -21,41 +22,51 @@ export default function Vans() {
         })
     }
 
-    const typeFilter = searchParams.get("type");
+    function renderVans(allVans) {
+        const uniqueTypes = [...new Set(allVans.map((el) => el.type))];
+        const filterBar = uniqueTypes.map(el =>
+            // <button onClick={ () => setSearchParams(`?type=${el}`)}>{el}</button>
+            <button onClick={() => handleFilterChange(`type`, `${el}`)}>{el}</button>
+        )
 
-    const uniqueTypes = [...new Set(allVans.map((el) => el.type))];
-    const filterBar = uniqueTypes.map(el =>
-        // <button onClick={ () => setSearchParams(`?type=${el}`)}>{el}</button>
-        <button onClick={() => handleFilterChange(`type`, `${el}`)}>{el}</button>
-    )
+        const displayedVans = typeFilter ?
+            allVans.filter(el => el.type === typeFilter) : allVans;
 
-    const displayedVans = typeFilter ?
-        allVans.filter(el => el.type === typeFilter) : allVans;
-
-    const elementsToDisplay = displayedVans.map(el =>
-        <Link to={`./${el.id}`} state={{ search: `?${searchParams.toString()}`, type: typeFilter }}
-            aria-label={`View details for ${el.name}, priced at ${el.price} per day`}>
-            <div key={el.id} className="van--container">
-                <img src={el.imageUrl} alt={`Image of ${el.name}`} />
-                <div>
-                    <h3>{el.name}</h3>
-                    <p>€{el.price}<span>/day</span></p>
-                    <i className="van--type"><span className="badge">{el.type}</span></i>
+        const elementsToDisplay = displayedVans.map(el =>
+            <Link to={`./${el.id}`} state={{ search: `?${searchParams.toString()}`, type: typeFilter }}
+                aria-label={`View details for ${el.name}, priced at ${el.price} per day`}>
+                <div key={el.id} className="van--container">
+                    <img src={el.imageUrl} alt={`Image of ${el.name}`} />
+                    <div>
+                        <h3>{el.name}</h3>
+                        <p>€{el.price}<span>/day</span></p>
+                        <i className="van--type"><span className="badge">{el.type}</span></i>
+                    </div>
                 </div>
-            </div>
-        </Link>
-    )
+            </Link>
+        )
+
+        return (
+            <>
+                <div className="filter-bar--vans">
+                    {filterBar}
+                    {typeFilter && <button onClick={() => handleFilterChange("type", null)}>Clear filters</button>}
+                </div>
+                <div className="vans--container">
+                    {elementsToDisplay}
+                </div>
+            </>
+        )
+    }
 
     return (
         <div>
             <h2>Explore our van options</h2>
-            <div className="filter-bar--vans">
-                {filterBar}
-                {typeFilter && <button onClick={() => handleFilterChange("type", null)}>Clear filters</button>}
-            </div>
-            <div className="vans--container">
-                {elementsToDisplay}
-            </div>
-        </div>
+            <Suspense fallback={<h2>Loading...</h2>}>
+                <Await resolve={promiseData.vans}>
+                    {renderVans}
+                </Await>
+            </Suspense>
+        </div >
     )
 }
